7c5cba0c6375ad63da42fb80339d374855cb6eca
Wala AL mawali <wala.salam.9888@gmail.com>
2026-05-05 11:15:08 +0400

    Complete MenuMessages class

--- src/Utils/MenuMessages.java
+++ src/Utils/MenuMessages.java
@@ -1,4 +1,43 @@
 package Utils;
 
 public class MenuMessages {
+
+    public static String MAIN_MENU_MESSAGE = """
+            1- Library Menu
+            2- Book Menu
+            3- Magazine Menu
+            4- member Menu
+            5- Exit
+            """;
+    public static String Book_MENU_MESSAGE = """
+            1- Add New Book
+            2- Show Book Details
+            3- Delete Book
+            4- Update Book
+            5- Go back to Main Menu
+            """;
+
+    public static String Magazine_MENU_MESSAGE = """
+            1- ADD New Magazine
+            2- Show Magazine Details
+            3- Delete Magazine
+            4- Update Magazine
+            5- Go Back to Main Menu
+            """;
+    public static String Member_MENU_MESSAGE = """
+            1- Register Member
+            2- Show Member Details
+            3- Delete Member
+            4- Update Member
+            5- Go Back to Main Menu
+            """;
+    public static String Library_MENU_MESSAGE = """
+            1- Display All Books
+            2- Display All Magazines
+            3- DISPLAY All Members
+            4- Search For Book
+            5- Search For Magazine
+            6- Search For Member
+            """;
+
 }
