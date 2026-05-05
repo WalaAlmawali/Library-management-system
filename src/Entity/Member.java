cd05102606129a8f4e77c45b0e96c56d0094286e
Wala AL mawali <wala.salam.9888@gmail.com>
2026-05-05 10:36:55 +0400

    Declare attripute of Member class and generate getter and setter function for all attripute

--- src/Entity/Member.java
+++ src/Entity/Member.java
@@ -1,7 +1,31 @@
 package Entity;
 
 public class Member {
+    private int id;
+    private String name;
+    private MemberAddress address;
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public MemberAddress getAddress() {
+        return address;
+    }
+
+    public void setAddress(MemberAddress address) {
+        this.address = address;
+    }
 }
