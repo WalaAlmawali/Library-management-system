ffe3059b9e9a598567f9968671ea3bddc721393e
Wala AL mawali <wala.salam.9888@gmail.com>
2026-05-05 10:27:31 +0400

    Declare attripute of parent class and generate getter and setter function for all attripute

--- src/Entity/Parent.java
+++ src/Entity/Parent.java
@@ -1,4 +1,32 @@
 package Entity;
 
 public class Parent {
+
+    private int Id;
+    private String title;
+    private boolean status;
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
