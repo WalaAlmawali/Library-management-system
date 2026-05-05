2ea5058404b93d9d7fb999569e948f2d224b3ab0
Wala AL mawali <wala.salam.9888@gmail.com>
2026-05-05 10:39:44 +0400

    Declare attripute of MemberAddress class and Declare the constructor

--- src/Entity/MemberAddress.java
+++ src/Entity/MemberAddress.java
@@ -1,4 +1,19 @@
 package Entity;
 
 public class MemberAddress {
+
+    private String street;
+    private String city;
+    private String postalCode;
+
+    public MemberAddress(String street, String city, String postalCode) {
+        this.street = street;
+        this.city = city;
+        this.postalCode = postalCode;
+    }
+
+    public String getFormattedAddress() {
+        return street + ", " + city + ", " + postalCode;
+    }
+
 }
